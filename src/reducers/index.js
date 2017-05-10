ab691fdfaf75ae354118bed600ebc0039b46bd72
Jules <jferreira@bamboohr.com>
2017-05-10 13:15:32 -0600

    update reducers to use/return immutable objects

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,23 @@
-import { combineReducers } from 'redux'
 import todos from './todos'
 import visibilityFilter from './visibilityFilter'
 
+// for immutable
+import { combineReducers } from 'redux-immutable'
+import { Record, List } from 'immutable'
+
+// state stored as immutable Record composed of List and string
+const InitialState = Record({
+	todos: List(),
+	visibilityFilter: 'SHOW_ALL'
+})
+
+// root reducer
 const todoApp = combineReducers({
 	todos,
 	visibilityFilter
-})
+},
+	// pass initial state to root reducer
+	InitialState
+)
 
 export default todoApp
--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,32 +1,39 @@
+// for immutable
+import { Record } from 'immutable'
+
+// individual todos stored as Record
+const TodoRecord = Record({
+	id: null,
+	text: '',
+	completed: false
+})
+
+// nested reducer gets `TodoRecord` as state from `todos` (parent reducer)
 const todo = (state, action) => {
 	switch (action.type) {
 		case 'ADD_TODO':
-			return {
+			// new record with non-default id and text
+			return TodoRecord({
 				id: action.id,
 				text: action.text,
-				completed: false
-			}
+			})
 		case 'TOGGLE_TODO':
 			if (state.id !== action.id) {
+				// not the todo you're looking for..
 				return state
 			}
-
-			return {
-				...state,
-				completed: !state.completed
-			}
+			return state.set('completed', !state.completed)
 		default:
 			return state
 	}
 }
 
-const todos = (state = [], action) => {
+// reduces `todos` List from root reducer
+const todos = (state, action) => {
 	switch (action.type) {
 		case 'ADD_TODO':
-			return [
-				...state,
-				todo(undefined, action)
-			]
+			// push `TodoRecord` from nested reducer into List
+			return state.push(todo(undefined, action))
 		case 'TOGGLE_TODO':
 			return state.map(t =>
 				todo(t, action)
